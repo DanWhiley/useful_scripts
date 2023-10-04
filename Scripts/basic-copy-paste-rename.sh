8ae758fb02d8e725141531db6f42928e8f102924
DanWhiley <146936821+DanWhiley@users.noreply.github.com>
2023-10-04 12:29:51 +0100

    basic-copy-paste-rename.sh

---
+++ Scripts/basic-copy-paste-rename.sh
@@ -0,0 +1,7 @@
+## This is a very basic copy file command to a new shared location while renaming the file. This is useful for tools that output multiple directories such as assembly tools. The benefit of a basic command like this is to spot any syntax mistakes. 
+
+cp '/home/isolate_1/contigs.fasta' '/home/all_contigs/SUD100_contigs.fasta' ;
+cp '/home/isolate_2/contigs.fasta' '/home/all_contigs/SUD101_contigs.fasta' ;
+cp '/home/isolate_3/contigs.fasta' '/home/all_contigs/SUD102_contigs.fasta' ;
+cp '/home/isolate_4/contigs.fasta' '/home/all_contigs/SUD103_contigs.fasta' ;
+cp '/home/isolate_5/contigs.fasta' '/home/all_contigs/SUD104_contigs.fasta'
