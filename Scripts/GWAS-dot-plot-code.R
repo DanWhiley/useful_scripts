0a26f2663640df9a1e5e516b7b2a052424f97ce1
DanWhiley <146936821+DanWhiley@users.noreply.github.com>
2023-10-09 14:06:01 +0100

    GWAS-dot-plot-code.R

---
+++ Scripts/GWAS-dot-plot-code.R
@@ -0,0 +1,21 @@
+## Load libraries 
+require(ggplot2)
+require(ggrepel)
+library(ggrepel)
+
+## Load gene hits file from GWAS output
+
+gene_hits = read.table("gene_hits.txt", stringsAsFactors=FALSE, header=TRUE)
+
+# Plot the results as a dot plot using colour blind friendly palatte. At the same time, write an appropriate title. 
+
+ggplot(gene_hits, aes(x=avg_beta, y=maxp, colour=avg_maf, size=hits, label=gene)) +
+    geom_point(alpha=0.5) +
+    geom_text_repel(aes(size=1.4), show.legend = FALSE, colour='black') +
+    scale_size("Number of k-mers", range=c(1,10)) +
+    scale_colour_gradient('Average MAF') +
+    theme_bw(base_size=11) +
+    ggtitle("GWAS - milk phenotype") +
+    xlab("Average effect size") +
+    ylab("Maximum -log10(p-value)") + theme_classic() -> d
+d + scale_color_viridis_c()
